2ff3213ad3b89a2f3903f3935de5e06b60dc50db
Romdoni Agung Purbayanto <doni@stockbit.com>
2017-11-22 17:09:41 +0700

    add profile en & es

--- src/main/java/dondon/springframework/dependencyinjection/services/PrimaryGreetingService.java
+++ src/main/java/dondon/springframework/dependencyinjection/services/PrimaryGreetingService.java
@@ -1,15 +1,17 @@
 package dondon.springframework.dependencyinjection.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
+@Profile("en")
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hellp from Primary Greeting Service";
+        return "Primary Greeting Service";
     }
 
 }
--- src/main/java/dondon/springframework/dependencyinjection/services/PrimarySpanishGreetingService.java
+++ src/main/java/dondon/springframework/dependencyinjection/services/PrimarySpanishGreetingService.java
@@ -1,4 +1,17 @@
 package dondon.springframework.dependencyinjection.services;
 
-public class PrimarySpanishGreetingService {
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Servicio de Saludo Primario";
+    }
+
 }
