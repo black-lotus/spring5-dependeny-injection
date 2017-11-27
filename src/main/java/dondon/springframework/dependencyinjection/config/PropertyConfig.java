b7b56dde2f4849cba90a1fe55eda4687d54512ed
Romdoni Agung Purbayanto <doni@stockbit.com>
2017-11-27 15:13:40 +0700

    use environment variable

--- src/main/java/dondon/springframework/dependencyinjection/config/PropertyConfig.java
+++ src/main/java/dondon/springframework/dependencyinjection/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package dondon.springframework.dependencyinjection.config;
 
 import dondon.springframework.dependencyinjection.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment environment;
 
     @Value("${fake.username}")
     String user;
@@ -23,6 +28,10 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
+
+        // user environment variable
+        // fakeDataSource.setUser(environment.getProperty("FAKE_USERNAME"));
+
         fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
